bb0fb4c9929d165d99d01cb40edbe4f0bbade1d4
m-ghanem-dev <mohamedgh4444@gmail.com>
2026-01-03 14:49:02 +0100

    use major version 14

--- database.tf
+++ database.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   engine              = "postgres"
-  engine_version      = "14.5"
+  engine_version      = "14"
   instance_class      = "db.t3.micro"
   db_name             = var.db_name
   username            = var.db_username
